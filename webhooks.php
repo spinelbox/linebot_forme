b8adf84e73a2458bf025e530b68d1a63828bb2b9
spinelbox <spinelbox@gmail.com>
2018-08-29 14:59:12 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -7,5 +7,9 @@ require 'line-bot-sdk-php-master/line-bot-sdk-tiny/LINEBotTiny.php';
 <?php
   $content = file_get_contents('php://input');
   $events = json_decode($content,true);
+
+  var_dump($content);
+  echo "<br/>";
+  var_dump($events);
   echo "again!";
 ?>
