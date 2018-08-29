2cce5a722db12949584ea313eb5367cba9272305
cat <spinelbox@gmail.com>
2018-08-29 14:46:16 +0700

    update

--- webhooks.php
+++ webhooks.php
@@ -1,5 +1,13 @@
 <h1>Yoo!</h1>
 <?php
 require "config.php";
-require_once("line-bot-sdk-php-master/line-bot-sdk-tiny/LineBotTiny.php");
+require_once ("line-bot-sdk-php-master/line-bot-sdk-tiny/LineBotTiny.php");
+?>
+
+<?php
+  $content = file_get_contents("php://input");
+  $events = json_decode($content,true);
+
+  var_dump($content);
+  var_dump($events);
 ?>
