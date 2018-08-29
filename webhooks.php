d35ef4b4b24fcd2ddeeca7b6ba5dcb7a6d1c2fd6
spinelbox <spinelbox@gmail.com>
2018-08-29 14:39:05 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -5,7 +5,7 @@ require_once("line-bot-sdk-php-master/line-bot-sdk-tiny/LineBotTiny.php");
 ?>
 
 <?php
-  $content = file_get_contents("php://input");
+  //$content = file_get_contents("php://input");
   echo $content;
   print_r($content);
   echo date("H:i:s");
